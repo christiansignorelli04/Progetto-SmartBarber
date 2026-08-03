12a23e1e3cba263b39e1556043a3fdad788e9b84
christiansignorelli04 <christiansignorelli04@gmail.com>
2026-08-03 18:47:31 +0200

    Primo commit setup iniziale

---
+++ src/main/java/com/smartbarber/smartbarber/SmartbarberApplication.java
@@ -0,0 +1,13 @@
+package com.smartbarber.smartbarber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbarberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbarberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartbarber/smartbarber/SmartbarberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbarber.smartbarber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbarberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
